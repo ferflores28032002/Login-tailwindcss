424e8d1fd90e44396cedf0ddd0935ec438a31d1b
fernando flores <martinjanns2000@gmail.com>
2022-11-04 10:59:39 -0600

    commit numero 2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <div className="w-full min-h-screen bg-[#695cfe] flex justify-center items-center">
      
-      <div className="max-w-md py-8 p-10 shadow-2xl rounded bg-white">
+      <div className="max-w-md mx-2 py-8 p-10 shadow-2xl rounded bg-white">
 
         <img src={logo2} alt="logo de la empresa" />
 
